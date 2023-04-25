2314ac55fca65e636fada6af90c91587c662ec99
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-04-25 09:39:02 +0300

    style: add margin on mobile  nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
       <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden' : 'fixed left-[-100%] md:hidden'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>LOGO.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4 mt-7'>LOGO.</h1>
         <ul className='uppercase p-4'>
           <li className='p-4 border-b border-gray-600'>Suaugusiems</li>
           <li className='p-4 border-b border-gray-600'>Vaikams</li>
